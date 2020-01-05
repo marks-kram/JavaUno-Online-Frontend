9471fefc802e7471a149c2b744c39d0317159447
tomatenmark <mh88@mailbox.org>
2020-01-05 20:52:44 +0100

    added actions

--- res/js/app-turn.js
+++ res/js/app-turn.js
@@ -24,3 +24,110 @@ function getCardImage(card, size) {
 
     return image;
 }
+
+function put(card, index){
+    if(isPutAllowed(card)){
+        const data = {
+            card: card,
+            cardIndex: index,
+            gameUuid: app.gameUuid,
+            playerUuid: app.playerUuid
+        };
+        doPostRequest('/action/put', data, loadGame);
+    }
+}
+
+function putDrawn(){
+    const cardIndex = app.gameState.ownCards.length-1;
+    const card = app.gameState.ownCards[cardIndex];
+    put(card, cardIndex);
+}
+
+function draw(){
+    if(isDrawAllowed()){
+        doAction('draw');
+    }
+}
+
+function keep(){
+    doAction('keep');
+}
+
+function selectColor(color){
+    const path = '/action/select-color/' + app.gameUuid + '/' + app.playerUuid + '/' + color;
+    doPostRequest(path, {}, loadGame);
+}
+
+function sayUno(){
+    app.toast = 'Du: „Uno“';
+    doAction('say-uno');
+}
+
+function isPutAllowed(card){
+    if(!isMyTurn()){
+        return false;
+    }
+    if(!isPlayable(card)){
+        return false;
+    }
+    return app.gameState.game.turnState === 'PUT_OR_DRAW' ||
+        app.gameState.game.turnState === 'PUT_DRAWN' || app.gameState.game.turnState === 'DRAW_DUTIES_OR_CUMULATIVE';
+}
+
+function isDrawAllowed() {
+    if(!isMyTurn()){
+        return false;
+    }
+    if(app.gameState.game.turnState === 'PUT_DRAWN'){
+        return false;
+    }
+    if(app.gameState.game.turnState === 'SELECT_COLOR'){
+        return false;
+    }
+    return app.gameState.game.turnState !== 'FINAL_COUNTDOWN';
+}
+
+function isSayUnoAllowed() {
+    if(!isMyTurn()){
+        return false;
+    }
+    return app.gameState.game.turnState === 'SELECT_COLOR' || app.gameState.game.turnState === 'FINAL_COUNTDOWN';
+}
+
+function isMyTurn(){
+    return app.gameState.game.currentPlayerIndex === app.gameState.myIndex;
+}
+
+function doAction(action){
+    const path = '/action/' + action + '/' + app.gameUuid + '/' + app.playerUuid;
+    doPostRequest(path, {}, loadGame);
+}
+
+function isPlayable(card){
+    if(app.gameState.game.turnState === 'DRAW_DUTIES_OR_CUMULATIVE' && !isCumulative(app.gameState.game.topCard, card)){
+        return false;
+    }
+    return isMatch(app.gameState.game.topCard, card);
+}
+
+function isCumulative(topCard, card){
+    return topCard.drawValue === card.drawValue;
+}
+
+function isMatch(topCard, card){
+    if(card.jokerCard){
+        return true;
+    }
+    switch(topCard.cardType){
+        case 'NUMBER':
+            return card.value === topCard.value || card.color === topCard.color;
+        case 'SKIP':
+        case 'REVERSE':
+        case 'DRAW_2':
+            return card.cardType === topCard.cardType || card.color === topCard.color;
+        case 'JOKER':
+        case 'DRAW_4':
+            return card.color === app.gameState.game.desiredColor;
+    }
+    return false;
+}
